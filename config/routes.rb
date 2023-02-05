cd20550c4862d48fa7f85c05d77d3aa0d6f85ea9
EC2 Default User <ec2-user@ip-172-31-18-41.ap-northeast-1.compute.internal>
2023-02-05 11:52:39 +0000

    [Add]resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
-  get 'books/new'
-  post 'books' => 'books#create'
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show'
-  get 'books/:id' => 'books#edit', as: 'book'
 
+  resources :books
 end
 
