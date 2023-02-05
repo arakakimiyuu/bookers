78710dc368d2825019b30ed6b14f6f64fb61d816
EC2 Default User <ec2-user@ip-172-31-18-41.ap-northeast-1.compute.internal>
2023-02-05 13:01:11 +0000

    [Add]updateに@,creteにif

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,11 +4,15 @@ class BooksController < ApplicationController
   end
 
   def create
-     book = Book.new(book_params)
+     @book = Book.new(book_params)
 
-     book.save
+    if @book.save
 
-   　redirect_to book_path(book.id)
+     redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render:index
+    end
   end
 
   def index
@@ -26,14 +30,14 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book.update(book_params)
+    redirect_to book_path(@book)
   end
 
   def destroy
-    @book = Book.find(params[:id])
+    book = Book.find(params[:id])
     book.destroy
-    redirect_to book_path(book.id)
+    redirect_to books_path
   end
 
   private
